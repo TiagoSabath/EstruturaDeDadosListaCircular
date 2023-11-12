6ce8154965f637191d4ce6a4aa41c9d605df12b5
Tiago Sabath <tiago.sabath@gmail.com>
2023-11-12 06:56:49 -0300

    Initial commit

---
+++ ListaCircular/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+    ListaCircular <String> listaCircular = new ListaCircular<>();
+
+    listaCircular.add("c0");
+    listaCircular.add("c1");
+    listaCircular.add("c2");
+    listaCircular.add("c3");
+    listaCircular.add("c4");
+    listaCircular.add("c5");
+
+        System.out.println(listaCircular);
+    }
+}
